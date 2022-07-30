d02f19f9e6fc039013dddee5d2ac999b8642e94e
luiizsps <103710492+luiizsps@users.noreply.github.com>
2022-07-29 22:07:48 -0300

    Final Commit
    projeto finalizado

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
-// código fonte do problema 2 proposto em pd2 prática, onde realiza-se a inscrição de usuarios em um processo seletivo
-
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct candidatos {
+// Struct que armazena os cadastros
+typedef struct candidatos { 
 	char nome[40];
 	int idade;
 	int area;
@@ -14,15 +13,14 @@ typedef struct candidatos {
 	struct candidatos *next; 
 }dados;
 
+// struct que armazena o endere�o do primeiro elemento da lista e n�mero de inscri��es
 typedef struct lista {
 	dados *start;
 	int total;
 }List;
 
 void inscricao(List *list);
-//void ranking(*p);
 void ranking(List *list);
-void print(List *list);
 void buscar_remover(List *list);
 
 void main() {
@@ -37,16 +35,17 @@ void main() {
 		inscricao(&list);
 		list.total++;
 
-		printf("Realizar mais uma inscrição? ");
+		printf("Realizar mais uma inscri��o? ");
 		fgets(curso, 5, stdin);
 		fflush(stdin);
 	}while(curso[0] != 'N' && curso[0] != 'n');
 
-	buscar_remover(&list);	
+	buscar_remover(&list);
 	ranking(&list);
-	//print(&list);
+
 }
 
+// Fun��o que aplica o question�rio, contabiliza as pontua��es e armazena em listas encadeadas
 void inscricao(List *list) {
 	int pont;
 	char duracao[10], curso[5];
@@ -57,7 +56,7 @@ void inscricao(List *list) {
 	fgets(aux->nome, 40, stdin);
 	fflush(stdin);
 
-	printf("CPF(somente numeros): ");
+	printf("CPF (somente n�meros): ");
 	scanf("%d", &aux->CPF);
 	getchar();
 	
@@ -65,18 +64,18 @@ void inscricao(List *list) {
 	scanf("%d", &aux->idade);
 	getchar();
 		
-	printf("Área pretendida:\n[1] Engenharia de Software\n[2] Banco de Dados\n[3] Análise e Projeto de Algoritmos\n");
+	printf("�rea pretendida:\n[1] Engenharia de Software\n[2] Banco de Dados\n[3] An�lise e Projeto de Algoritmos\n");
 	scanf("%d", &aux->area);
 	getchar();
 
-	printf("Jï¿½ realizou algum curso na ï¿½rea? ");
+	printf("J� realizou algum curso na �rea? ");
 	fgets(curso, 5, stdin);
 	fflush(stdin);
 
 	aux->pontos = 0;
 		
 	while (curso[0] == 's'|| curso[0] == 'S') {
-		printf("De curta duraï¿½ï¿½o(atï¿½ 10 horas), mï¿½dia duraï¿½ï¿½o(de 11 ï¿½ 40 horas) ou longa duraï¿½ï¿½o (mais de 40 horas)? ");
+		printf("De curta dura��o (at� 10 horas), m�dia dura��o (de 11 � 40 horas) ou longa dura��o (mais de 40 horas)? ");
 		fgets(duracao, 10, stdin);
 		fflush(stdin);
 			
@@ -99,7 +98,7 @@ void inscricao(List *list) {
 				
 			aux->pontos += pont*3;
 		}
-		printf("Possui mais algum curso na ï¿½rea?");
+		printf("Possui mais algum curso na �rea? ");
 		fgets(curso, 5, stdin);
 		fflush(stdin);	
 	}
@@ -116,7 +115,7 @@ void inscricao(List *list) {
 		aux->pontos += pont*2;
 	}
 		
-	printf("Possui algum curso de lï¿½ngua estrangeira? ");
+	printf("Possui algum curso de l�ngua estrangeira? ");
 	fgets(curso, 5, stdin);
 	fflush(stdin);
 		
@@ -127,7 +126,7 @@ void inscricao(List *list) {
 
 		aux->pontos += pont*2;
 	} 
-	printf("Pesquisa ou trabalha na ï¿½rea pretendida a mais de 3 anos? ");
+	printf("Pesquisa ou trabalha na �rea pretendida a mais de 3 anos? ");
 	fgets(curso, 5, stdin);
 	fflush(stdin);
 		
@@ -135,7 +134,7 @@ void inscricao(List *list) {
 		aux->pontos +=1;
 	}
 		
-	printf("Possui certificaï¿½ï¿½es nacionais ou internacionais? ");
+	printf("Possui certifica��es nacionais ou internacionais? ");
 	fgets(curso, 5, stdin);
 	fflush(stdin);
 		
@@ -143,7 +142,7 @@ void inscricao(List *list) {
 		aux->pontos +=1;
 	}
 		
-	printf("Jï¿½ participou de alguma equipe de desenvolvimento de software para o governo? ");
+	printf("J� participou de alguma equipe de desenvolvimento de software para o governo? ");
 	fgets(curso, 5, stdin);
 	fflush(stdin);
 		
@@ -155,46 +154,51 @@ void inscricao(List *list) {
 	list->start = aux;
 }
 
-//void ranking(*p) {
+// Fun��o que gera e imprime o raking, al�m de imprimir o total e incri��es e o total de incri��es em cada �rea
 void ranking(List *list) {
 	
 	int i=0;
 	char nomes_an[2][100], nomes_eng[2][100],nomes_bc[2][100];
-	int posicoes[6], maior_an=0, maior_eng=0, maior_bc=0;
+	int posicoes[6], maior_an[2], maior_eng[2], maior_bc[2];
 	int tam_an = 0, tam_eng = 0, tam_bc=0;
 	dados  *aux;
 	
 	aux = list->start;
 	
+	for (i=0;i<2;i++){
+		maior_an[i]=0;
+		maior_eng[i]=0;
+		maior_bc[i]=0;
+	}
+	i=0;
+	
 	if (aux == NULL) {
-		printf("erro de memoria.");
+		printf("Lista vazia.\n");
 	}
 	else {
+		//La�o que encontra as maiores pontua��es por �rea
 		while(aux != NULL) {
 			if(aux->area == 3){			
-				if(aux->pontos > maior_an){
+				if(aux->pontos > maior_an[0]){
 					posicoes[0] = i;
-					maior_an = aux->pontos;
+					maior_an[0] = aux->pontos;
 					strcpy(nomes_an[0], aux->nome);
-				//	nomes_an[0]= aux[posicoes[0]].nome;
 				}
 				tam_an +=1;
 			}
 			if(aux->area == 1){			
-				if(aux->pontos > maior_eng){
+				if(aux->pontos > maior_eng[0]){
 					posicoes[2] = i;
-					maior_eng = aux->pontos;
+					maior_eng[0] = aux->pontos;
 					strcpy(nomes_eng[0], aux->nome);
-				//	nomes_eng[0]= aux[posicao[2]].nome;
 				}
 				tam_eng +=1;
 			}
 			if(aux->area == 2){			
-				if(aux->pontos > maior_bc){
+				if(aux->pontos > maior_bc[0]){
 					posicoes[4] = i;
-					maior_bc = aux->pontos;
+					maior_bc[0] = aux->pontos;
 					strcpy(nomes_bc[0], aux->nome);
-				//	nomes_bc[0]= aux[posicao[4]].nome;
 				}
 				tam_bc +=1;
 			}
@@ -207,42 +211,37 @@ void ranking(List *list) {
 		
 		if(i>1){
 			i=0;
-			maior_an=0;
+			//La�o que encontra as segundas maiores pontua��es por �rea
 			while(aux != NULL){
 				
 				if(aux->area == 3) {	
 					if(tam_an>1) {
 						
-						if(aux->pontos > maior_an && posicoes[0]!=i){
+						if(aux->pontos > maior_an[1] && posicoes[0]!=i){
 							posicoes[1] = i;
-							maior_an = aux->pontos;
+							maior_an[1] = aux->pontos;
 							strcpy(nomes_an[1], aux->nome);
-						//	nomes_an[1]= aux[posicao[1]].nome;
 						}
 					}
 				}
 				if(aux->area == 1){	
-					if(tam_eng>1)	{
+					if(tam_eng>1) {
 					
-						if(aux->pontos > maior_an && posicoes[2]!=i){
+						if(aux->pontos > maior_eng[1] && posicoes[2]!=i) {
 							posicoes[3] = i;
-							maior_an = aux->pontos;
+							maior_eng[1] = aux->pontos;
 							strcpy(nomes_eng[1], aux->nome);
-		
-					//		nomes_eng[1]= aux[posicao[3]].nome;
 						}
 					}
 				}
 				if(aux->area == 2){	
 					if(tam_bc>1){
 					
-						if(aux->pontos > maior_an && posicoes[4]!=i){
+						if(aux->pontos > maior_bc[1] && posicoes[4]!=i){
 							posicoes[5] = i;
-							maior_an = aux->pontos;
+							maior_bc[1] = aux->pontos;
 							strcpy(nomes_bc[1], aux->nome);
-		
-						//	nomes_bc[1]= aux[posicao[5]].nome;
-						}
+								}
 					}
 				}
 				i+=1;
@@ -251,42 +250,38 @@ void ranking(List *list) {
 		}
 		aux = list->start;
 		if(aux!=NULL) {
+			printf("Total de candidatos inseridos: %d\n", i);
+			printf("Total de candidatos de An�lise e Projeto de Algoritmos: %d\n", tam_an);
+			printf("Total de candidatos de Engenharia de Software: %d\n", tam_eng);
+			printf("Total de candidatos de Banco de Dados: %d\n", tam_bc);
+
 			printf("\nRanking: \n");
 			if(tam_an>0) {
-				printf("Análise e Projeto de Algoritmos: \n");
-				printf("1 - %s", nomes_an[0]);
+				printf("An�lise e Projeto de Algoritmos: \n");
+				printf("1 - %s: %d pontos\n", nomes_an[0], maior_an[0]);
 				if (tam_an>1) {
-					printf("2 - %s", nomes_an[1]);
+					printf("2 - %s: %d pontos\n", nomes_an[1], maior_an[1]);
 				}
 			}
 			if(tam_eng>0) {
 				printf("Engenharia de Software: \n");
-				printf("1 - %s", nomes_eng[0]);
+				printf("1 - %s: %d pontos\n", nomes_eng[0], maior_eng[0]);
 				if(tam_eng>1) {
-					printf("2 - %s", nomes_eng[1]);
+					printf("2 - %s: %d pontos\n", nomes_eng[1], maior_eng[1]);
 				}
 			}
 			if(tam_bc>0) {
 				printf("Banco de Dados: \n");
-				printf("1 - %s", nomes_bc[0]);
+				printf("1 - %s: %d pontos\n", nomes_bc[0], maior_bc[0]);
 				if(tam_bc>1) {
-					printf("2 - %s", nomes_bc[1]);
+					printf("2 - %s: %d pontos\n", nomes_bc[1], maior_bc[1]);
 				}
 			}
 		}
 	}	
 }
-//}
-void print(List *list) {
-	dados *aux = list->start;
 
-	while (aux != NULL) {
-		printf("Nome: %s\n",aux->nome);
-		printf("Protocolo: %d\n",aux->CPF);
-		aux = aux->next;
-	}
-}
-
+// Fun��o que realiza a busca e remo��o de uma incri��o atrav�s do CPF
 void buscar_remover(List *list) {
 	
 	int x, del, remove;
@@ -298,7 +293,7 @@ void buscar_remover(List *list) {
 	scanf("%d", &x);
 	
 	if (aux == NULL) {
-		printf("erro de memoria.");
+		printf("Lista vazia.\n");
 	}
 	else {
 		while(aux != NULL && aux->CPF != x) {
@@ -307,9 +302,9 @@ void buscar_remover(List *list) {
 		}
 
 		if (aux != NULL) {
-			printf("Elemento encontrado 1\n");
+			printf("Candidato encontrado\n");
 
-			printf("Deseja remover a incriï¿½ï¿½o? ");
+			printf("Deseja remover a incri��o?\n[0] N�o\n[1] Sim\n");
 			scanf("%d", &remove); 
 			getchar();
 
@@ -326,21 +321,20 @@ void buscar_remover(List *list) {
 				}
 
 				aux = list->start;
-
+				// Procura pela inscri��o removida, verificando se a remo��o foi efetiva
 				while(aux != NULL && aux->CPF != x) {
 					aux = aux->next;
 				}
 				if(aux != NULL) {
-					printf("elemento não removido.");
+					printf("Candidato n�o removido.\n");
 				}
 				else {
-					printf("elemento removido");
+					printf("Candidato removido.\n");
 				}
 			}
-
 		}
 		else {
-			printf("Elemento não encontrado 1");
+			printf("Candidato n�o encontrado.\n");
 		}
 	}
 }
